3ada83fd8289afa03a30ab1d2b1dd16e5247ca13
Oron Segev <segev.oron@gmail.com>
2020-07-30 17:59:31 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'SignInBestParctice';
+  passwordShowToggle: 'text' | 'password' = 'password';
+
+  signInForm = new FormGroup({
+    email: new FormControl('',[Validators.required]),
+    password: new FormControl('')
+  });
+
+  constructor() {}
+
+  togglePassword() {
+    if (this.passwordShowToggle === 'text') {
+      this.passwordShowToggle = 'password';
+    } else {
+      this.passwordShowToggle = 'text';
+    }
+  }
 }
